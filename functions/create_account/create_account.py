9267a9169731eedf21a9fc5344c348575fb63b4f
loganrw <loganrwatkins@hotmail.com>
2024-06-14 19:12:32 -0500

    adding auth lambda and pipeline to deploy lambda

--- functions/create_account/create_account.py
+++ functions/create_account/create_account.py
@@ -1,22 +1,35 @@
 import boto3
+import json
 
-client = boto3.client("cognito-idp")
 
+def lambda_handler(event, context):
 
-response = client.admin_create_user(
-    UserPoolId="string",
-    Username="string",
-    UserAttributes=[
-        {"Name": "string", "Value": "string"},
-    ],
-    ValidationData=[
-        {"Name": "string", "Value": "string"},
-    ],
-    TemporaryPassword="string",
-    ForceAliasCreation=True | False,
-    MessageAction="RESEND" | "SUPPRESS",
-    DesiredDeliveryMediums=[
-        "EMAIL",
-    ],
-    ClientMetadata={"string": "string"},
-)
+    first_name = event.get("firstName")
+    last_name = event.get("lastName")
+    email = event.get("email")
+    password = event.get("password")
+
+    client = boto3.client("cognito-idp")
+
+    response = client.sign_up(
+        '6gaiajd80v2srhhs1jcj2v8dmc',
+        Username=email,
+        Password=password,
+        UserAttributes=[
+            {
+                'Name': 'email',
+                'Value': email
+
+            },
+            {
+                'Name': 'custom:first_name',
+                'Value': first_name
+            },
+            {
+                'Name': 'custom:last_name',
+                'Value': last_name
+            }
+        ],
+    )
+
+    print(response)
