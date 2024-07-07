8c6e25cd8c183f739e2bbb030ae3e4904d2d1319
loganrw <superposition.games.studio@gmail.com>
2024-07-07 01:24:00 -0500

    added confirm code lambda

--- functions/authorize_user/authorize_user.py
+++ functions/authorize_user/authorize_user.py
@@ -1,5 +1,18 @@
 import boto3
 import json
 
+
 def lambda_handler(event, context):
-    print(event)
+
+    username = event.get("userName")
+    confirmCode = event.get("confirmCode")
+
+    client = boto3.client("cognito-idp")
+
+    response = client.confirm_sign_up(
+        ClientId="4pnr3uhqa32fnjhrqoc4lqob0n",
+        Username=username,
+        ConfirmationCode=confirmCode,
+    )
+
+    print(response)
